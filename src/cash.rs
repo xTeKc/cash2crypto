d47dec49f7520a580a367aa0e8ffbe56b666bd46
xTeKc <bcd3v@pm.me>
2022-02-20 20:02:50 -0500

    update cash enum: add currencies

--- src/cash.rs
+++ src/cash.rs
@@ -1,8 +1,14 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Cash {
     Afghani,    // AFN  ؋
+    AusDollar,  // AUD  $
+    Dinar,      // DZD  د.ج
+    Dram,       // AMD  ֏
     Euro,       // EUR  €
+    Kwanza,     // AOA  Kz
     Lek,        // ALL  L
+    Manat,      // AZN  ₼
+    Peso,       // ARS  $
     Pound,      // GBP  £
     Ruble,      // RUB  ₽
 }
