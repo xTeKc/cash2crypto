da5a1c4542fe6285ae7256eece9075bbd60da208
xTeKc <bcd3v@pm.me>
2022-02-21 00:50:14 -0500

    add imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod cash;
+use iced::text_input::{self, TextInput};
+use iced::window;
+use iced::{
+    pick_list, scrollable, Align, Container, Element, HorizontalAlignment, Length, PickList,
+    Sandbox, Scrollable, Settings, Text,
+};
+use cash::Cash;
+
 fn main() {
-    println!("Hello, world!");
+
 }
